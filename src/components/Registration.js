23e01504efb51948c937d90ff73641452042e034
Sunday Omolewu <omolewu12@gmail.com>
2023-01-17 10:25:24 +0100

    state for the form

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -8,29 +8,99 @@ function Registration() {
         marginTop: '2%'
     }
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] =useState('');
-    const [password, setPassword] = useState('');
+    const [user, setuser] = useState({
+      name: '',
+      email : '',
+      phone: '',
+      password : '',
+    });
+    const [errors, setErrors] = useState({
+      nameErr: '',
+      emailErr: '',
+      phoneErr: '',
+      password: ''
+    })
+    const handleForm = (e)=>{
+       let name= e.target.name;
+       let value = e.target.value;
+       setuser({
+        ...user,
+        [name]: value,
+       })
+    }
+    const submitData = (e)=>{
+      e.preventDefault()
 
+      if(user.name == "" || user.name == 0 ||user.name == null){
+        setErrors((err)=>{
+        return  {
+            ...err,
+            nameErr: `Name is required`,
+          }
+        })
+      }
+      if(user.email == "" || user.email == 0 ||user.email == null){
+        setErrors((err)=>{
+        return  {
+            ...err,
+            emailErr: `Email is required`,
+          }
+        })
+      }
+      if(user.phone == "" || user.phone == 0 ||user.phone == null){
+        setErrors((err)=>{
+        return  {
+            ...err,
+            phoneErr: `Phone is required`,
+          }
+        })
+      }
+      if(user.password == "" || user.password == 0 ||user.password == null){
+        setErrors((err)=>{
+        return  {
+            ...err,
+            passwordErr: `Password is required`,
+          }
+        })
+      }
+
+      console.log(user)
+    }
   return (
     <div>
-       <form style={mainForm }>
+       <form onSubmit={submitData} style={mainForm }>
         <div style={formDiv}> 
-        {name}
-            <input type="text" onChange={(e)=>{setName (e.target.value)}}
+        {user.name}
+            <input type="text" onChange={handleForm}
              name='name' placeholder='Your Name' 
               style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}}/>
+            <strong>{errors.nameErr}</strong>
           </div>
             <div style={formDiv}>
-            <input type="email" name='email' placeholder='Your Email'  style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}} />
+            {user.email}
+            <input type="email" name='email'
+            onChange={handleForm}
+             placeholder='Your Email' 
+              style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}} />
         </div>
         <div style={formDiv}>
-            <input type="tel" name='phone' placeholder='Your phone' style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}} />
+        {user.phone}
+            <input type="tel"
+             name='phone' onChange={handleForm}
+              placeholder='Your phone'
+               style={{width: '100%',
+                height:'40px', border:'none',
+                 borderBottom: '1px inset green'}} />
         </div>
         <div style={formDiv}>
-            <input type="password" name='password' placeholder='Your Password' style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}} />
+        {user.password}
+            <input type="password"
+             name='password'
+             onChange={handleForm}
+              placeholder='Your Password'
+               style={{width: '100%', height:'40px', border:'none', borderBottom: '1px inset green'}} />
         </div>
+        <button type='submit'>Register</button>
        </form>
 
     </div>
